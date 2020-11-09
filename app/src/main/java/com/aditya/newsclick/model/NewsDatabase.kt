99191f73b75b44cc4a5363a410e1b06b2d84ffec
aditya-bhawsar <aditya.bhawsar98@gmail.com>
2020-11-09 11:40:34 +0530

    Initial Commit

--- app/src/main/java/com/aditya/newsclick/model/NewsDatabase.kt
+++ app/src/main/java/com/aditya/newsclick/model/NewsDatabase.kt
@@ -4,8 +4,4 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 
 @Database(entities = [Article::class], version = 1)
-abstract class NewsDatabase :RoomDatabase(){
-
-    abstract fun getNewsDao():NewsDao
-
-}
+abstract class NewsDatabase :RoomDatabase(){ abstract fun getNewsDao():NewsDao }
